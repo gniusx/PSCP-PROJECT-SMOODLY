f9d242a0802b7f6b6e07dda64d62e8b6f6eead1a
Pleng_Nuttida <66070059@kmitl.ac.th>
2023-10-30 16:33:31 +0700

    big update [detail in des]
    -หน้า2 เพิ่มscript ไว้ส่งonclickไปหน้า 5 แต่ยังไม่เสร็จ
    <button onclick="mood('sad')">
    ใส่ไปแค่อันเดียวต้องใส่moodอื่นอีก
    -หน้า 3  ก้อปมาจากหน้า 2 พักไว้ก่อนรอทำหน้า 2 เสร็จค่อยมาแก้
    -หน้า 5 มีสุ่มข้อความให้กำลังใจแล้ว แต่ manual มาก ต้องเข้าไปกด run ใน Python ก่อนถึงจะขึ้นบนเว็บ

---
+++ page5 output_random text.py
@@ -0,0 +1,20 @@
+"""for testing randoming text from file"""
+#สมมติว่าเลือก angry
+import random
+angry_txt = 'text_output/Text_output_Angry (โกรธ).txt'
+#r อ่านข้อความในไฟล์
+with open(angry_txt, 'r') as file:
+    #ทำข้อความเป็นlist
+    eachline = file.readlines()
+#สุ่มจากlist
+random_t = random.choice(eachline)
+print(random_t)
+
+with open('page5 output.html', 'r') as show_output:
+    html_page5 = show_output.read()
+html_page5 = html_page5.replace("loading for random text...", random_t)
+
+with open('page5 output.html', 'w') as output_file:
+    output_file.write(html_page5)
+
+#แต่ต้องมากด run ก่อนอ่ะตอนนี้ ถึงจะขึ้นบนเว็บ
