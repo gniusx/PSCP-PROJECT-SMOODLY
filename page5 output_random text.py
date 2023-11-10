64a9bf7e33d2ee56b7a91609e093f8013b3bde7f
Pleng_Nuttida <66070059@kmitl.ac.th>
2023-11-10 17:24:12 +0700

    python ใส่ function แปลงค่าให้เป็นชื่อไฟล์

--- page5 output_random text.py
+++ page5 output_random text.py
@@ -1,11 +1,12 @@
 """for testing randoming text from file"""
 
-def receive_selected():
-    html_selected = request.get.data(as_text=True)
-    #ลองทำตามตัวอย่างอ่ะ แสดงค่าไปไฟล์text
-    with open('output.txt', 'w') as file:
-        file.write(html_selected)
-    return 'ส่งข้อมูลสำเร็จ'
+def trans_to_eng(thai_selected):
+    """เปลี่ยนค่าที่ดึงจากไทยเป็นอังกฤษ"""
+    thai_selected = thai_selected.split('เรื่อง')
+    mood = {'เศร้า':'sad','โกรธ':'angry', 'น่าเบื่อ':'bored', 'กังวล':'worry', 'มีความสุข':'happy'}
+    topic = {'ครอบครัว':'family','เพื่อน':'friends', 'ความรัก':'love', 'เรียน/งาน':'study-work', 'สุขภาพ':'health'}
+    eng_selected = '%s-%s' %(mood[thai_selected[0]], topic[thai_selected[1]])
+    return eng_selected
 
 def randomtext(selected):
     """เสิชชื่อไฟล์ที่ตรงกับ input มาทำ list"""
@@ -19,12 +20,15 @@ def randomtext(selected):
     random_t = random.choice(eachline)
     print(random_t)
 
-    with open('page5 output.html', 'r') as show_output:
-        html_page5 = show_output.read()
-    html_page5 = html_page5.replace("loading for random text...", random_t)
+    #ใส่ข้อความที่สุ่มได้ลงใน html
+    #ขอปิดไว้ก่อนน้า ถ้ากดไฟล์มันจะเปลี่ยนแล้วเปลี่ยนเลยอยู่น่ะ
+    
+    #with open('page5 output.html', 'r') as show_output:
+        #html_page5 = show_output.read()
+    #html_page5 = html_page5.replace("loading for random text...", random_t)
 
-    with open('page5 output.html', 'w') as output_file:
-        output_file.write(html_page5)
-randomtext("angry-family")
-#แต่ต้องมากด run ก่อนอ่ะตอนนี้ ถึงจะขึ้นบนเว็บ
+    #with open('page5 output.html', 'w') as output_file:
+        #output_file.write(html_page5)
+randomtext(trans_to_eng('น่าเบื่อเรื่องสุขภาพ'))
+#เวอร์ชั่นmanual ตรงด้านในวงเล็บของจริงจะเป็นค่าที่เราดึงมาจากเว็บ
 
