339c869be1e91c00dfa8cbc5976fbab29e547646
Pleng_Nuttida <66070059@kmitl.ac.th>
2023-11-11 17:04:01 +0700

    ✅can pass variable to python

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from pyscript import Element
 #def send_to_py(): 
     #Element("random_show").write("ทดสอบทดสอบ")
 
-def send_to_py(a): 
-    Element("random_show").write(f"{a}")
+def send_to_py(): 
+    
+    Element("random_show").write(f"{selected}")
 #กะว่าจะแยกกัน คือตรงbuttonส่ง selected เข้ามาให้ได้ 
 #แล้วปล่อยให้pyรัน ที่แปล สุ่ม นู่นนี่นั่น
 #ค่อยส่งกลับตอนสุ่มเสร็จ
